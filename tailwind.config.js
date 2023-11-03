063d9aeb9f79510b202f8d087515b49262ca6622
Himath_R <himath4510@gmail.com>
2023-11-03 22:56:09 +1000

    init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{html,js,jsx}",
+    "./components/**/*.{html,js,jsx}",
+    "./sections/**/*.{html,js,jsx}",
+    "./styles/**/*.{js,jsx}",
   ],
+  mode: "jit",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 6s ease infinite alternate",
+      },
+      colors: {
+        "primary-black": "#f3f4f6",
+        "white-background": "#f3f4f6",
+        "secondary-white": "#c7c7c7",
+        "text-body": "#181b25",
+        "text-heading": "#1f1c4f",
+        "westpac-red": "#DA1710",
+      },
+      transitionTimingFunction: {
+        "out-flex": "cubic-bezier(0.05, 0.6, 0.4, 0.9)",
       },
     },
   },
   plugins: [],
-}
+};
